b7642d6c07f6e62dea2aea1add1555be4cc8b7d6
raylan <raylan@rd-devone.attlocal.net>
2024-07-22 21:01:01 -0400

    placeholder header/footer created

---
+++ client/src/components/footer/footer.component.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './footer.styles.scss';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className='bottom-footer'>
+        <p> This is a footer </p>
+      </div>
+    )
+  }
+}
+
+export default Footer;
---
+++ client/src/components/header/header.component.tsx
@@ -0,0 +1,11 @@
+import './header.styles.scss';
+
+const Header: React.FC = (): JSX.Element => {
+  return (
+    <div className='top-header'>
+      <p> This is a header </p>
+    </div>
+  )
+}
+
+export default Header;
