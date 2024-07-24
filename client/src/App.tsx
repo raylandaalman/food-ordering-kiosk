8208f962bbf5213dd41d5aae2aaa85bc65f69d9f
raylandalman <raylan.git@daalman.dev>
2024-07-23 20:43:41 -0400

    button component partially done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import './App.scss';
 
 import Header from './components/header/header.component';
-
 import Footer from './components/footer/footer.component';
+import MainButtons from './components/mainButtons/mainButtons.component';
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
+      <MainButtons />
+      <img 
+        src={require('./images/main-food-drink.png')}
+        alt='food right drink left'
+      />
     </div>
   );
 }
---
+++ client/src/components/button/button.component.tsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import './mainButtons.styles.scss';
+
+interface ButtonProps {
+  text: string;
+  color: string;
+}
+
+interface Button_Type_Classes {
+  blue: string;
+  green: string;
+  gray: string;
+}
+
+const Button_Type_Classes<Button_Type_Classes> {
+  blue: 'blue-button',
+  green: 'green-button',
+  gray: 'gray-button'
+}
+
+class Button extends Component<ButtonProps> {
+  
+}
+
+export default MainButtons;
